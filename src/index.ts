3ca6599d863ea92a19ce7ae9c3f5dad6324e8d01
Kentent Roth <kentencroth@yahoo.com>
2024-07-14 14:42:50 -0500

    Adding player to game
    Need some major refactoring

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,13 @@ interface User {
 	created_at: string;
 }
 
+interface Game {
+	id: number;
+	name: string;
+	created_by_id: number;
+	created_at: string;
+}
+
 const app = new Hono<{ Bindings: Env }>();
 
 app.post('/signup', async (c) => {
@@ -68,11 +75,32 @@ app.post('/games', validateAccess, async (c) => {
 		.bind(data.name, getUserID.results[0].id)
 		.run();
 	if (!createGame) return c.json({ error: 'Error creating game' }, 500);
-	return c.json(createGame);
+	const gameId = createGame.meta.last_row_id;
+	const addPlayerResult = await c.env.DB.prepare('INSERT INTO players (user_id, game_id) VALUES (?, ?)')
+		.bind(getUserID.results[0].id, gameId)
+		.run();
+	if (!addPlayerResult) return c.json({ error: 'Error adding player to game' }, 500);
+
+	return c.json('Game Created');
 });
 
 // Create Player
-app.post('/players', validateAccess, async (c) => {});
+app.post('/players', validateAccess, async (c) => {
+	const data = await c.req.json();
+	const getCreatedByID = (await c.env.DB.prepare('SELECT id FROM users WHERE username = ?').bind(data.created_by).run()) as D1Result<User>;
+	if (getCreatedByID.results.length === 0) return c.json({ error: 'User not found' }, 404);
+	const getUserID = (await c.env.DB.prepare('SELECT id FROM users WHERE username = ?').bind(data.username).run()) as D1Result<User>;
+	if (getUserID.results.length === 0) return c.json({ error: 'User not found' }, 404);
+	const getGameID = (await c.env.DB.prepare('SELECT id FROM games WHERE name = ? AND created_by_id = ?')
+		.bind(data.game, getCreatedByID.results[0].id)
+		.run()) as D1Result<Game>;
+	if (getGameID.results.length === 0) return c.json({ error: 'Game not found' }, 404);
+	const createPlayer = await c.env.DB.prepare('INSERT INTO players (user_id, game_id) VALUES (?, ?)')
+		.bind(getUserID.results[0].id, getGameID.results[0].id)
+		.run();
+	if (!createPlayer) return c.json({ error: 'Error creating player' }, 500);
+	return c.json('Player Added');
+});
 
 // Create Trade
 app.post('/trades', validateAccess, async (c) => {});
